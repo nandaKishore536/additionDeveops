7e56a082f12f7289e84f39dae44b6e5a0c3b3697
AdmiN <AdmiN@ITWS0038>
2024-08-29 10:38:39 +0530

    addition operaton

---
+++ src/addition/Add.java
@@ -0,0 +1,14 @@
+package addition;
+
+public class Add {
+	
+	  public static void main(String[] args) {
+	      	int number1 = 10;
+	        int number2 = 20;
+	        
+	        int sum = number1 + number2;
+	        
+	        System.out.println("The sum of " + number1 + " and " + number2 + " is " + sum);
+}
+	  
+}
