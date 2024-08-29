7c7df9386b7edbbc5e8619b5834081fe4000862b
AdmiN <AdmiN@ITWS0040>
2024-08-29 11:18:42 +0530

    commit

--- src/addition/Add.java
+++ src/addition/Add.java
@@ -9,6 +9,17 @@ public class Add {
 	        int sum = number1 + number2;
 	        
 	        System.out.println("The sum of " + number1 + " and " + number2 + " is " + sum);
+}	  
 }
-	  
+class mul
+{
+	public static void main(String args[])
+	{
+		int number2 = 20;
+		int number1 = 10;
+        int mul = number1 + number2;
+        
+        System.out.println("The sum of " + number1 + " and " + number2 + " is " + mul);
+	}
 }
+
